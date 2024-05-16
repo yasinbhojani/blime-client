25f570559bb2f2dd10e40de5db01baf508f24910
SohamGanmote <sohamganmote@gmail.com>
2024-05-16 20:52:09 +0530

    APIs for auth inegrated with JWT

---
+++ src/http/post/auth/authAPIs.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export const handelLogin = async ({ email, password }) => {
+	let payload;
+
+	if (email.includes("@")) {
+		payload = {
+			email,
+			password,
+		};
+	} else {
+		payload = {
+			username: email,
+			password,
+		};
+	}
+
+	try {
+		const response = await axios.post(`${apiUrl}/auth/login`, payload);
+		let token = response.data.payload.accessToken;
+		localStorage.setItem("token", token);
+		return token;
+	} catch (error) {
+		console.error("Error fetching data: ", error);
+	}
+};
+
+export const handelRegister = async ({
+	firstName,
+	lastName,
+	email,
+	password,
+}) => {
+	const payload = {
+		first_name: firstName,
+		last_name: lastName,
+		email,
+		password,
+	};
+
+	try {
+		const response = await axios.post(
+			`${apiUrl}/auth/normal-registration`,
+			payload
+		);
+		let token = response.data.token;
+		localStorage.setItem("token", token);
+		return token;
+	} catch (error) {
+		console.error("Error fetching data: ", error);
+	}
+};
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,16 @@
-export const generateRandomNoFromRange = (startNum,endNum) => {
-  return Math.floor(Math.random() * (endNum - startNum + 1)) + startNum;
-} 
+export const generateRandomNoFromRange = (startNum, endNum) => {
+	return Math.floor(Math.random() * (endNum - startNum + 1)) + startNum;
+};
+
+export const decodeJWT = () => {
+	try {
+		const token = localStorage.getItem("token");
+		const payload = token.split(".")[1];
+		const decodedPayload = atob(payload);
+		const userObj = JSON.parse(decodedPayload);
+		return userObj.user_details;
+	} catch (error) {
+		console.error("Error decoding JWT token:", error);
+		return null;
+	}
+};
